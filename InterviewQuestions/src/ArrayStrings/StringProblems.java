2da776bec6e8154b986dd823295586c6fbbdc560
Alex Yang <gya10@sfu.ca>
2015-03-13 00:12:24 -0700

    Tomorrow's interview question practice: Given a number n, find the largest number just smaller than n that can be formed using the same digits as n.

---
+++ InterviewQuestions/src/ArrayStrings/StringProblems.java
@@ -0,0 +1,17 @@
+package ArrayStrings;
+
+public class StringProblems {
+
+	/* Given a number n, find the largest number just smaller than n 
+	 * that can be formed using the same digits as n.
+	 * Eg: 1342, return 1324*/
+	public int findLargestSmallerNumber(int n){
+		//TODO
+		
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		
+	}
+}
