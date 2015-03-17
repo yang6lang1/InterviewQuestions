9e9782bbc50f8d3b613c9699a71667c9e6aa9bf5
Alex Yang <gya10@sfu.ca>
2015-03-16 19:19:47 -0700

    Finished Friday's assignment. findLargestSmallerNumber. Idea: 1. start from the end of the number, check if the numbers are in decresing order from right to left (because numbers with digits combination such as 1234 are the numbers with the smallest value) 2. find the first digit that conflicts with this rule 174567: 17 <--- 0000111100 				16 + 77 + 5 + 4 				167754

--- InterviewQuestions/src/ArrayStrings/StringProblems.java
+++ InterviewQuestions/src/ArrayStrings/StringProblems.java
@@ -5,13 +5,51 @@ public class StringProblems {
 	/* Given a number n, find the largest number just smaller than n 
 	 * that can be formed using the same digits as n.
 	 * Eg: 1342, return 1324*/
-	public int findLargestSmallerNumber(int n){
-		//TODO
+	public int findLargestSmallerNumber(int n) throws Exception{
+		// check int valid
+		// check int has num with the correct answer: eg 1234
+		if(n < 0) throw new Exception("Negative number.");
 		
-		return 0;
+		int scale = 10;
+		int currDig = 0;
+		int[] status = new int[10];
+		int output = 0;
+		
+		outerLoop:
+		do{
+			currDig = n - (n / scale) * scale;
+			n /= scale;
+			for(int i = currDig - 1; i >= 0; i--){
+				if(status[i] > 0){
+					output = n * scale + i;
+					status[i]--;
+					status[currDig]++;
+					break outerLoop;
+				}
+			}
+			status[currDig]++;
+			if(n == 0){
+				throw new Exception("Invalid number.");
+			}
+		}
+		while(n != 0);
+		
+		for(int i = 9; i >= 0; i--){
+			while(status[i] > 0){
+				output = output * scale + i;
+				status[i] --;
+			}
+		}
+		 
+		return output;
 	}
 	
 	public static void main(String[] args){
-		
+		StringProblems sp =new StringProblems();
+		try {
+	    System.out.println(sp.findLargestSmallerNumber(174455));
+    } catch (Exception e) {
+	    System.out.println(e.getMessage());
+    }
 	}
 }
