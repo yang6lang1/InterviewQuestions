fa73f9b078137b55b189360e9db8d1858c605cad
Alex Yang <gya10@sfu.ca>
2015-07-05 12:45:02 -0700

    Updated

--- InterviewQuestions/src/ArrayStrings/ArrayProblems.java
+++ InterviewQuestions/src/ArrayStrings/ArrayProblems.java
@@ -2,25 +2,51 @@ package ArrayStrings;
 
 public class ArrayProblems {
 
-	public void removeDuplicates(char[] str){
-		if (str == null) return;
-		int len = str.length;
-		if (len < 2) return;
+	private void rotateMatrix(int[][] image){
+		if(image == null || image.length != image[0].length) return;
 		
-		int tail = 1;
-		for(int i = 0; i < str.length; i++){
-			int j = 0;
-			for(j = 0; j < tail; j++){
-				if(str[i] == str[j]) break;
-			}
-			if(j == tail){
-				str[tail] = str[i];
-				tail++;
+		int temp = 0;
+		int N = image.length;
+		for(int l = 0; l < N/2; l++){
+			for(int i = 0; i < (N - 1 - 2 * l); i++){
+				temp = image[l][l + i];
+				image[l][l + i] = image[N - 1 - l - i][l];
+				image[N - 1 - l - i][l] = image[N - 1 - l][N - 1 - l - i];
+				image[N - 1 - l][N - 1 - l - i] = image[l + i][N - 1 - l];
+				image[l + i][N - 1 - l] = temp;
 			}
 		}
 	}
 	
 	public static void main(String[] args) {
-
+		ArrayProblems ap = new ArrayProblems();
+		int N = 5;
+		int[][] image = new int[N][N];
+		int count = 0;
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				image[i][j] = count;
+				count++;
+			}
+		}
+		
+		//iamge before rotation
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				System.out.print(image[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		
+		ap.rotateMatrix(image);
+		
+		//image after rotation
+		System.out.println();
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				System.out.print(image[i][j] + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
