57e7d7e58063e6f7816d183fb23223957bc398db
Alex Yang <yg398701842@live.cn>
2015-07-06 12:51:34 -0700

    Update

--- InterviewQuestions/src/ArrayStrings/ArrayProblems.java
+++ InterviewQuestions/src/ArrayStrings/ArrayProblems.java
@@ -4,7 +4,7 @@ public class ArrayProblems {
 
 	private void rotateMatrix(int[][] image){
 		if(image == null || image.length != image[0].length) return;
-		
+
 		int temp = 0;
 		int N = image.length;
 		for(int l = 0; l < N/2; l++){
@@ -17,9 +17,39 @@ public class ArrayProblems {
 			}
 		}
 	}
-	
+
+	private void matrixCheck(int[][] matrix){
+		if(matrix == null) return;
+
+		boolean[] rowFlags = new boolean[matrix.length];
+		boolean[] colFlags = new boolean[matrix[0].length];
+
+		for(int r = 0; r < matrix.length; r++){
+			for(int c = 0; c < matrix[0].length; c++){
+				if(matrix[r][c] == 0){
+					rowFlags[r] = true;
+					colFlags[c] = true;
+				}
+			}
+		}
+
+		for(int r = 0; r < matrix.length; r++){
+			for(int c = 0; c < matrix[0].length; c++){
+				if(rowFlags[r]){
+					matrix[r][c] = 0;
+					continue;
+				}
+				if(colFlags[c]){
+					matrix[r][c] = 0;
+				}
+			}
+		}
+	}
+
+
 	public static void main(String[] args) {
 		ArrayProblems ap = new ArrayProblems();
+
 		int N = 5;
 		int[][] image = new int[N][N];
 		int count = 0;
@@ -29,7 +59,7 @@ public class ArrayProblems {
 				count++;
 			}
 		}
-		
+
 		//iamge before rotation
 		for(int i = 0; i < N; i++){
 			for(int j = 0; j < N; j++){
@@ -37,14 +67,46 @@ public class ArrayProblems {
 			}
 			System.out.println();
 		}
-		
+
 		ap.rotateMatrix(image);
-		
+
 		//image after rotation
 		System.out.println();
 		for(int i = 0; i < N; i++){
 			for(int j = 0; j < N; j++){
 				System.out.print(image[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+
+		int M = 4;
+		int[][] matrix = new int[M][N];
+		count = 0;
+		for(int i = 0; i < M; i++){
+			for(int j = 0; j < N; j++){
+				matrix[i][j] = count;
+				count++;
+			}
+		}
+		matrix[1][2] = 0;
+
+		System.out.println();
+		//matrix before manipulation
+		for(int i = 0; i < M; i++){
+			for(int j = 0; j < N; j++){
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
+
+		ap.matrixCheck(matrix);
+
+		//image after rotation
+		System.out.println();
+		for(int i = 0; i < M; i++){
+			for(int j = 0; j < N; j++){
+				System.out.print(matrix[i][j] + "\t");
 			}
 			System.out.println();
 		}
